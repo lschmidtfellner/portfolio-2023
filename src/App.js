2abc9b57be0d586e45296052b73562d100e1a3b9
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2025-02-17 12:23:07 -0500

    finished making navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Bg from './components/Bg'
-import Nav from './components/Nav'
-import Landing from './pages/Landing'
-import Rippl from './pages/Rippl'
-import Newor from './pages/Newor'
-import Crisol from './pages/Crisol'
-import Vertrag from './pages/Vertrag'
-import ComingOfAge from './pages/ComingOfAge'
-import MetalCoat from './pages/MetalCoat'
+import Bg from './components/Bg';
+import Landing from './pages/Landing';
+import Rippl from './pages/Rippl';
+import Newor from './pages/Newor';
+import Crisol from './pages/Crisol';
+import Vertrag from './pages/Vertrag';
+import ComingOfAge from './pages/ComingOfAge';
+import MetalCoat from './pages/MetalCoat';
 import RevRadar from './pages/RevRadar';
 import BreakpointIndicator from './components/BreakpointIndicator';
 
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import React, { useState, useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { ReactComponent as SiteLogo } from './assets/sitelogo.svg';
+import { ReactComponent as SiteLogo2 } from './assets/sitelogo2.svg';
+import DarkSwitch from './components/DarkSwitch';
 
 function App() {
 
@@ -22,14 +24,10 @@ function App() {
   const [pColor, setPColor] = useState('#101457');
 
   useEffect(() => {
-    if (modeToggle) {
-      setPColor('#F7B69F');
-    } else {
-      setPColor('#101457');
-    }
+    setPColor(modeToggle ? '#F7B69F' : '#101457');
   }, [modeToggle]);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleNavigation = (path) => {
     return new Promise((resolve) => {
@@ -37,7 +35,6 @@ function App() {
         top: 0,
         behavior: 'smooth'
       });
-
       setTimeout(() => {
         resolve();
       }, 500);
@@ -48,22 +45,38 @@ function App() {
 
   return (
     <div className="App" style={{ position: 'relative', zIndex: 1 }}>
-      <Bg modeToggle={modeToggle}/>
-      <Nav modeToggle={modeToggle} setModeToggle={setModeToggle} handleNavigation={handleNavigation}  pColor={pColor} />
+      <Bg modeToggle={modeToggle} />
+      <nav
+        className="fixed nav-div z-50 h-10 top-3 left-0 right-0 mx-6 sm:mx-20 md:mx-32 lg:mx-48 xl:mx-64 2xl:mx-80 mt-5 mb-4 pb-8 flex justify-between items-center"
+        style={{ borderBottom: `1px solid ${pColor}` }}
+      >
+        {modeToggle ? (
+          <SiteLogo2 
+            onClick={() => handleNavigation('/')} 
+            className="h-4 cursor-pointer" 
+          />
+        ) : (
+          <SiteLogo 
+            onClick={() => handleNavigation('/')} 
+            className="h-4 cursor-pointer" 
+          />
+        )}
+        <DarkSwitch modeToggle={modeToggle} setModeToggle={setModeToggle} pColor={pColor} />
+      </nav>
+      <div className="spacer pt-16"/>
       <Routes>
-        <Route path="/" element={<Landing modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />}></Route>
-        <Route path="/rippl" element={<Rippl modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />}></Route>
-        <Route path="/newor" element={<Newor modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation}/>}></Route>
-        <Route path="/crisol" element={<Crisol modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation}/>}></Route>
-        <Route path="/vertrag" element={<Vertrag modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation}/>}></Route>
-        <Route path="/comingofage" element={<ComingOfAge modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation}/>}></Route>
-        <Route path="/metalcoat" element={<MetalCoat modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation}/>}></Route>
-        <Route path="/metalcoat" element={<MetalCoat modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation}/>}></Route>
-        <Route path="/revradar" element={<RevRadar modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation}/>}></Route>
+        <Route path="/" element={<Landing modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
+        <Route path="/rippl" element={<Rippl modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
+        <Route path="/newor" element={<Newor modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
+        <Route path="/crisol" element={<Crisol modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
+        <Route path="/vertrag" element={<Vertrag modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
+        <Route path="/comingofage" element={<ComingOfAge modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
+        <Route path="/metalcoat" element={<MetalCoat modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
+        <Route path="/revradar" element={<RevRadar modeToggle={modeToggle} pColor={pColor} handleNavigation={handleNavigation} />} />
       </Routes>
       {/* <BreakpointIndicator/> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
